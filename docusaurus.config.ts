f32325d5435fa2bb6a4ec37831b28393e8178534
David Carvalho <david123car7@gmail.com>
2026-08-10 11:30:20 +0100

    feat: inserted the production and base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://crzvrc-docs.com',
+  url: 'https://David123car7.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/crzvrc-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
