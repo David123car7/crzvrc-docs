b97952b84ffeb9cfa7ad351b07f597b7687cc241
David Carvalho <david123car7@gmail.com>
2026-08-24 21:56:51 +0100

    feat: room system docs created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import {Redirect} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/docs/documentation/welcome" />;
+  return <Redirect to={useBaseUrl('/docs/documentation/welcome')} />;
 }
